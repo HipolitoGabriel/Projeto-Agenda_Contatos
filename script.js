43786e93beba5325f6cc4d040ce5efdae0704365
Gabriel Hipolito de Araujo <20241214010009@ifrn.local>
2026-06-30 16:24:46 -0300

    editar e excluir em desenvolvimento

--- script.js
+++ script.js
@@ -5,21 +5,20 @@ const buttonsend = document.querySelector("#buttonsend")
 const teste = document.querySelector("#teste")
 const dados = document.querySelector(".boxbody")
 const limpar = document.querySelector("#limpar")
-const botoes = document.querySelectorAll(".btnexcluir")
-const tam = document.querySelector("#tam")
 const pesquisa = document.querySelector("#navbar")
 
 button.addEventListener("click", open_popup);
 buttonsend.addEventListener("click", verificar)
+dados.addEventListener("click", excluir)
+dados.addEventListener("click", editar)
 
 const tabelaBody = document.getElementById("corpoTabela"); 
 
 function atualizarTabela(contato, indice) {
     
         const linhaHTML = `
-       <div class="contact">
+       <div class="contact" data-id="${indice}">
                 <div class="container">
-                <p>${indice}</p>
                 <h2>${contato.nome}</h3>
                 <ul>
                     <li>${contato.tel}</li>
@@ -28,8 +27,8 @@ function atualizarTabela(contato, indice) {
                 </ul>
             </div>
             <div class="seletores">
-                <button id="btneditar">Editar</button>
-                <button class="btnexcluir" data-id="${indice}">Excluir</button>
+                <button class="btneditar">Editar</button>
+                <button class="btnexcluir">Excluir</button>
             </div>
         </div>
         `;
@@ -45,14 +44,14 @@ function validar_email(email) {
     return email.includes("@") && email.includes(".")
 }
 function validar_num(tel) {
-    return /^\(\d{2}\) \d{9}$/.test(tel);
+    return /* /^\(\d{2}\) \d{9}$/.test(tel); */ true
 }
-function verificar(){
+function verificar() {
     const nome = document.getElementById("nome").value
     const tel = document.getElementById("tel").value
     const email = document.getElementById("email").value.trim()
     if (validar_nome(nome) === true && validar_email(email) === true && validar_num(tel) === true){
-        cadastrar(nome, tel, email)
+        cadastrar(nome, tel, email) 
     } else {
         alert("erro no nome, tel ou email");
     }
@@ -67,7 +66,6 @@ function cadastrar(nome, tel, email){
     lista_contatos.push(novo)
     atualizarTabela(novo, id)
     popup.close()
-    tam.innerHTML = botoes.length
     document.getElementById("nome").value = "";
     document.getElementById("tel").value = "";
     document.getElementById("email").value = "";
@@ -79,8 +77,8 @@ function pesquisar() {
     const filtadros = lista_contatos.filter((contato) => contato.nome.startsWith(foco))
         filtadros.forEach((contato, indice) => {
             const contato_filtadro = `
-        <div class="contact">
-            <div class="container">
+       <div class="contact" data-id="${indice}">
+                <div class="container">
                 <h2>${contato.nome}</h3>
                 <ul>
                     <li>${contato.tel}</li>
@@ -89,14 +87,36 @@ function pesquisar() {
                 </ul>
             </div>
             <div class="seletores">
-                <button id="btneditar">Editar</button>
-                <button class="btnexcluir" data-id="${indice}">Excluir</button>
+                <button class="btneditar">Editar</button>
+                <button class="btnexcluir">Excluir</button>
             </div>
         </div>
         `;
         dados.innerHTML += contato_filtadro
         })
 }
-function editar() {
-    
+const tam = document.querySelector("#tam")
+function excluir(e) {
+    const click = e.target
+    if (click.classList.contains("btnexcluir")){
+        const div_del = click.closest(".contact")
+        const id = Number(div_del.dataset.id)
+        div_del.remove()
+        tam.textContent = lista_contatos.length
+        lista_contatos.splice(id, 1)
+    }
+}
+function editar(e) {
+    const nome = document.getElementById("nome")
+    const tel = document.getElementById("tel")
+    const email = document.getElementById("email")
+    const click = e.target
+    if (click.classList.contains("btneditar")){
+        const id = Number(click.closest(".contact").dataset.id)
+        card_contato = lista_contatos[id]
+        popup.showModal()
+        nome.value = card_contato.nome
+        tel.value = card_contato.tel
+        email.value = card_contato.email
+    }
 }
